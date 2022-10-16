bb15cad82d9fc883831a4e84c306c4224802d60c
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-10-16 15:38:32 -0400

    after: After

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Envelope, Lock } from 'phosphor-react'
+import { Button } from './components/Button'
+import { Checkbox } from './components/Checkbox'
+import { Heading } from './components/Heading'
+import { Text } from './components/Text'
+import { TextInput } from './components/TextInput'
+import { Logo } from './Logo'
+import { SignIn } from './pages/SignIn'
 import './styles/global.css'
 
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <SignIn />
+  )
 }
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,16 +1,12 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
-import { ReactNode } from 'react';
 
-import { clsx } from 'clsx';
 import { Check } from 'phosphor-react';
 
-export interface CheckboxProps {
-  children: ReactNode;
-}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps { }
 
-export function Checkbox({ }: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+    <CheckboxPrimitive.Root {...props} className="w-6 h-6 p-[2px] bg-gray-800 rounded"
     >
       <CheckboxPrimitive.Indicator asChild>
         <Check weight='bold' className='h-5 w-5 text-cyan-300' />
